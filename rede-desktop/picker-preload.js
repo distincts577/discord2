5eec4418b1da11a5bcf233c677b8af222c5eabab
distincts577 <distincts577@gmail.com>
2026-08-23 21:25:26 -0300

    Delete rede-desktop directory

--- rede-desktop/picker-preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('pickerAPI', {
-  onSources: (callback) => ipcRenderer.on('picker-sources', (_event, sources) => callback(sources)),
-  choose: (sourceId) => ipcRenderer.send('picker-choose', sourceId),
-  cancel: () => ipcRenderer.send('picker-cancel'),
-});
