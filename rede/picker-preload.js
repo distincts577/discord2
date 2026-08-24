69727126a83efd22d453865545447041ddf30cf8
distincts577 <distincts577@gmail.com>
2026-08-24 16:47:00 -0300

    Delete rede directory

--- rede/picker-preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('pickerAPI', {
-  onData: (callback) => ipcRenderer.on('picker-data', (_event, data) => callback(data)),
-  confirm: (settings) => ipcRenderer.send('picker-choose', settings),
-  cancel: () => ipcRenderer.send('picker-cancel'),
-});
